2cf3b6b9110a7732f8eafd275fa871488b3d0fc0
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-05-31 14:17:19 +0200

    add secondo esercizio

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@
 // creazione variabile di registro
 var regUser = ['gino@gmail.com', 'pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
 
+// step 1
 var actualUser = prompt('Inserisci la tua email');
 
 var found = 'L\'email inserita non è registrata all\'interno del database';
 
+// step 2
 for(var i = 0; i < regUser.length; i++) 
 {
     if(actualUser == regUser[i]){
@@ -17,6 +19,42 @@ for(var i = 0; i < regUser.length; i++)
     }
 }
 
-//mostra risultato controllo
+// step 3 - mostra risultato controllo
 document.getElementById('mail').innerHTML = found;
-console.log(found);
+// console.log(found);
+
+
+
+
+
+//-------------------------------------------------------------------------------------------
+// Lista Cognomi:
+// 1 - chiedi all’utente il cognome
+// 2 - inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3 - stampa la lista ordinata alfabeticamente
+// 4 - scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+
+var regSurname = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+// console.log(regSurname);
+
+// step 1
+var userSurname = prompt('Inserisci il tuo cognome');
+
+// step 2
+regSurname.push(userSurname);
+console.log(regSurname);
+
+// step 3
+regSurname.sort();
+
+// step 4
+var pos = 0;
+for (var i = 0; i < regSurname.length; i++) {
+    if (userSurname == regSurname[i]){
+        pos = i;
+    }
+}
+
+console.log(pos);
+console.log(regSurname);
